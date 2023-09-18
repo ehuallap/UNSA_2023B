5e8a3c61af1a576e6f7bc240936fc7862ee4ac95
Erick Hualla <83515313+ehuallap@users.noreply.github.com>
2023-09-18 08:47:07 -0500

    Add files via upload

---
+++ ComputaciónParaleltaYDistribuida/lab01.cpp
@@ -0,0 +1,47 @@
+/***************************************************************
+    * AUTOR: ERICK KLENDORT HUALLA PUELLES
+    * CURSO: COMPUTACION PARALELA Y DISTRIBUIDA (UNSA 2023 B)
+    * ACTIVIDAD: LABORATORIO 01 - Comparación de desempeño - bucles anidados.
+    
+    ESTE CÓDIGO ES COMPLETAMENTE DE MI AUTORÍA
+***************************************************************/
+
+#include <iostream>
+#include <cstring>
+#include <chrono>
+using namespace std;
+
+#define MAX 500
+
+int main() {
+    int i, j;
+    double A[MAX][MAX], x[MAX], y[MAX];
+
+    /* Initialize A and x, assign y = 0 */
+    memset(A, 2, sizeof(A));
+    memset(x, 3, sizeof(x));
+    memset(y, 0, sizeof(y));
+    
+    auto start_time = std::chrono::high_resolution_clock::now();
+    /* First pair of loops */
+    for (i = 0; i < MAX; i++)
+        for (j = 0; j < MAX; j++)
+            y[i] += A[i][j]*x[j];
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+
+    cout << "PRIMER BUCLE CON: " << duration.count() << " milisegundos" << endl;
+    
+    /* Assign y = 0 */
+    memset(y, 0, sizeof(y));
+
+    start_time = std::chrono::high_resolution_clock::now();
+    /* Second pair of loops */
+    for (j = 0; j < MAX; j++)
+        for (i = 0; i < MAX; i++)
+            y[i] += A[i][j]*x[j];
+    end_time = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+
+    cout << "SEGUNDO BUCLE CON: " << duration.count() << " milisegundos" << endl;
+}
